9f0efb7c5ec7e9f4671c3c9793f4b177740633ad
Fatih Olcay <fatiholcayjava@gmail.com>
2025-09-27 20:23:18 +0300

    logo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,29 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+                    System.out.println("Hello World!");
+
+
+                    System.out.println();
+                    System.out.println("FFFFFFFFFFFFFFFFFFFFFFF          ");
+                    System.out.println("F::::::::::::::::::::F           ");
+                    System.out.println("F::::::::::::::::::::F           ");
+                    System.out.println("FF::::::FFFFFFFFF::::F           ");
+                    System.out.println("  F:::::F       FFFFFF           ");
+                    System.out.println("  F:::::F                        ");
+                    System.out.println("  F::::::FFFFFFFFFF              ");
+                    System.out.println("  F:::::::::::::::F              ");
+                    System.out.println("  F:::::::::::::::F         OOOO ");
+                    System.out.println("  F::::::FFFFFFFFFF       OO::::OO");
+                    System.out.println("  F:::::F                OO:::::::OO");
+                    System.out.println("  F:::::F                OO:::::::OO");
+                    System.out.println("FF:::::::FF               OO::::OO ");
+                    System.out.println("F::::::::FF                 OOOO   ");
+                    System.out.println("FFFFFFFFFF                         ");
+                    System.out.println();
+                    System.out.println("===================================");
+                }
         }
-    }
+
+
+
