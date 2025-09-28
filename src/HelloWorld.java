27231653387ff061eca902f8167852dcb1fe2de0
Fatih Olcay <fatiholcayjava@gmail.com>
2025-09-28 17:37:17 +0300

    HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,24 +5,19 @@ public class HelloWorld {
                     System.out.println("Hello World!");
 
 
-                    System.out.println();
-                    System.out.println("FFFFFFFFFFFFFFFFFFFFFFF          ");
-                    System.out.println("F::::::::::::::::::::F           ");
-                    System.out.println("F::::::::::::::::::::F           ");
-                    System.out.println("FF::::::FFFFFFFFF::::F           ");
-                    System.out.println("  F:::::F       FFFFFF           ");
-                    System.out.println("  F:::::F                        ");
-                    System.out.println("  F::::::FFFFFFFFFF              ");
-                    System.out.println("  F:::::::::::::::F              ");
-                    System.out.println("  F:::::::::::::::F         OOOO ");
-                    System.out.println("  F::::::FFFFFFFFFF       OO::::OO");
-                    System.out.println("  F:::::F                OO:::::::OO");
-                    System.out.println("  F:::::F                OO:::::::OO");
-                    System.out.println("FF:::::::FF               OO::::OO ");
-                    System.out.println("F::::::::FF                 OOOO   ");
-                    System.out.println("FFFFFFFFFF                         ");
-                    System.out.println();
-                    System.out.println("===================================");
+        System.out.println("            (  )   (   )  )            ");
+        System.out.println("             ) (   )  (  (             ");
+        System.out.println("             ( )  (    ) )             ");
+        System.out.println("             _____________             ");
+        System.out.println("            <_____________> ___        ");
+        System.out.println("            |             |/ _ \\      ");
+        System.out.println("            |               | | |      ");
+        System.out.println("            |               |_| |      ");
+        System.out.println("            |             |\\___/      ");
+        System.out.println("             \\___________/            ");
+        System.out.println();
+        System.out.println("             ☕  JAVA  ☕             ");
+        System.out.println("======================================");
                 }
         }
 
